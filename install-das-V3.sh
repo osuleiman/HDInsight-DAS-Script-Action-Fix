12dbfa8a6a1746503e4eed45ece832c7baa365da
osuleiman <34806809+osuleiman@users.noreply.github.com>
2019-11-14 17:03:35 +0200

    Update install-das-V3.sh

--- install-das-V3.sh
+++ install-das-V3.sh
@@ -76,7 +76,7 @@ fi
 #install service
 curl --retry 5 --retry-delay 15 -u $USERID:$PASSWD -H "X-Requested-By: ambari" -i -X PUT -d '{"ServiceInfo": {"state" : "INSTALLED"}, "RequestInfo": {"context": "Install Data Analytics Studio"}}' https://$CLUSTERNAME-int.azurehdinsight.net/api/v1/clusters/$CLUSTERNAME/services/DATA_ANALYTICS_STUDIO
 
-sleep 10s
+sleep 120s
 #start DATA ANALYTICS STUDO, retry 3 times if fails
 n=0
 SUCCESSCODE=202
